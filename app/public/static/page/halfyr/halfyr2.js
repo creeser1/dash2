c7b679b3fd57783c321698aeb389d20011c404fd
Reeser <creeser@calstate.edu>
2016-10-20 07:21:00 -0700

    changed defaults

--- app/public/static/page/halfyr/halfyr2.js
+++ app/public/static/page/halfyr/halfyr2.js
@@ -5,12 +5,11 @@
 	};
 	var create_chart = function (config, data) {
 		var fmt1 = function (y,x) {
-			//return this.series.name + ': ' + this.y + '%\<br />Year: ' + this.point.name;
-			//console.log(this);
 			var yearnum = config.period.slice(0,1);
-			return 'Year: ' + this.points[0].point.name + '<br />' + this.points[0].point.dif + ' students graduated <br />only one semester beyond ' + yearnum + ' years';
+			return 'Year: ' + this.points[0].point.name + '<br />' + this.points[0].point.dif + ' students graduated <br />only one term beyond ' + yearnum + ' years';
 		};
-		var txt1 = 'Grad Rate %';
+		var txt1 = 'Graduation Rate (%)';
+		var txt2 = '';
 		$('#chart1').highcharts({
 			credits: {
 				enabled: false
@@ -21,13 +20,14 @@
 				height: 550
 			},
 			title: {
-				text: 'The 2.5-year graduation rate is consistently 11 to 14  percentage points higher than the 2-year rate.'
-			},
-			subtitle: {
 				text: ''
 			},
 			xAxis: {
 				type: 'category',
+				title: {
+					text: 'Cohort Year',
+					align: 'middle'
+				},
 				labels: {
 					style: {
 						fontSize: '13px',
@@ -47,7 +47,6 @@
 				formatter: fmt1
 			},
 			legend: {
-				title: {style: {'color': '#777'}, text: ''},
 				layout: 'horizontal',
 				align: 'center',
 				verticalAlign: 'bottom',
@@ -66,10 +65,7 @@
 				marker: {radius: 10, symbol: "circle"},
 				dataLabels: {enabled: true, padding: 15, format: '{y:.0f}%'}}
 			},
-			series: data /*[
-				{"name": "2 Year Grad Rate", "color": "#37a", "data": [{"name": "2000", "y": 22.0}, {"name": "2001", "y": 22.0}, {"name": "2002", "y":  22.3}, {"name": "2003", "y":   23.4}, {"name": "2004", "y":  25.0}, {"name": "2005", "y":  24.1}, {"name": "2006", "y":  24.1}, {"name": "2007", "y":  23.1}, {"name": "2008", "y":  23.7}, {"name": "2009", "y":  24.8}, {"name": "2010", "y":  27.8}, {"name": "2011", "y":  26.8}, {"name": "2012", "y":  28.4}]},
-				{"name": "2.5 Year Grad Rate", "color": "#7a3", "data": [{"name": "2000", "y": 33.0}, {"name": "2001", "y": 32.7}, {"name": "2002", "y":  33.6}, {"name": "2003", "y":  34.9}, {"name": "2004", "y":  36.0}, {"name": "2005", "y":  35.7}, {"name": "2006", "y":  35.5}, {"name": "2007", "y":  34.2}, {"name": "2008", "y":  35.6}, {"name": "2009", "y":  37.0}, {"name": "2010", "y":  41.0}, {"name": "2011", "y":  40.3}, {"name": "2012", "y":  41.9}]},
-			]*/
+			series: data
 		});
 	};
 
@@ -109,24 +105,22 @@
 		var yr65 = [];
 		var ultimate_year = years.slice(-1)[0];
 		var penultimate_year = years.slice(-2,-1)[0];
-		//console.log(JSON.stringify([ultimate_year, penultimate_year]));
 		years.forEach(function (yr) {
 			var row = data[yr];
-			//console.log(JSON.stringify(row));
 			if (config.type === 'tr') {
 				yr2.push({"name": yr, "y": row[0][3], "dif": row[0][0]});
 				yr25.push({"name": yr, "y": row[0][4], "dif": row[0][0]});
-				yr3.push({"name": yr, "y": row[1][3], "dif": row[1][0]});
-				yr35.push({"name": yr, "y": (yr === ultimate_year ? null :row[1][4]), "dif": row[1][0]});
-				yr4.push({"name": yr, "y": (yr === ultimate_year ? null : row[2][3]), "dif": row[2][0]});
+				yr3.push({"name": yr, "y": (yr === ultimate_year ? null : row[1][3]), "dif": row[1][0]});
+				yr35.push({"name": yr, "y": (yr === ultimate_year ? null : row[1][4]), "dif": row[1][0]});
+				yr4.push({"name": yr, "y": (yr === ultimate_year || yr === penultimate_year ? null : row[2][3]), "dif": row[2][0]});
 				yr45.push({"name": yr, "y": (yr === ultimate_year || yr === penultimate_year ? null : row[2][4]), "dif": row[2][0]});
 			} else {
 				yr4.push({"name": yr, "y": row[2][3], "dif": row[2][0]});
 				yr45.push({"name": yr, "y": row[2][4], "dif": row[2][0]});
-				yr5.push({"name": yr, "y": row[3][3], "dif": row[3][0]});
+				yr5.push({"name": yr, "y": (yr === ultimate_year ? null : row[3][3]), "dif": row[3][0]});
 				yr55.push({"name": yr, "y": (yr === ultimate_year ? null : row[3][4]), "dif": row[3][0]});
-				yr6.push({"name": yr, "y": (yr === ultimate_year ? null : row[4][3]), "dif": row[4][0]});
-				yr65.push({"name": yr, "y":  (yr === ultimate_year || yr === penultimate_year ? null :row[4][4])});
+				yr6.push({"name": yr, "y": (yr === ultimate_year || yr === penultimate_year ? null : row[4][3]), "dif": row[4][0]});
+				yr65.push({"name": yr, "y":  (yr === ultimate_year || yr === penultimate_year ? null : row[4][4]), "dif": row[4][0]});
 			}
 		});
 		switch (config.period) {
@@ -155,16 +149,12 @@
 				output.push({"name": "2.5 Year Grad Rate", "color": "#3c5", "data": yr25});
 				break;
 		}
-		//console.log(JSON.stringify(years));
-		//console.log(JSON.stringify(output));
 		return output;
 	};
 	
 	var get_campus_data = function (config, callback) {
-		//console.log(JSON.stringify(config));
 		load_data(config, function (result) {
 			var selection = result[config.campus][config.type];
-			//console.log(JSON.stringify(selection));
 			var output = transform_data(config, selection);
 			//var test_output = [
 			//	{"name": "2 Year Grad Rate", "color": "#37a", "data": [{"name": "2000", "y": 22.0}, {"name": "2001", "y": 22.0}, {"name": "2002", "y":  22.3}, {"name": "2003", "y":   23.4}, {"name": "2004", "y":  25.0}, {"name": "2005", "y":  24.1}, {"name": "2006", "y":  24.1}, {"name": "2007", "y":  23.1}, {"name": "2008", "y":  23.7}, {"name": "2009", "y":  24.8}, {"name": "2010", "y":  27.8}, {"name": "2011", "y":  26.8}, {"name": "2012", "y":  28.4}]},
@@ -175,13 +165,10 @@
 	};
 
 	var populate_filter3 = function (config, data) {
-		//console.log(JSON.stringify(config));
 		var selection = data[config.campus][config.type];
-		//console.log(JSON.stringify(selection));
 		var years = Object.keys(selection);
 		var ok = {};
 		years.forEach(function (yr) {
-			//console.log(JSON.stringify([yr, selection[yr]]));
 			if (selection[yr][0][3] !== null) {
 				ok['2yr'] = true;
 			}
@@ -198,7 +185,6 @@
 				ok['6yr'] = true;
 			}
 		});
-		//console.log(JSON.stringify(ok));
 		var options = [];
 		var selectedval;
 		var opts = {'2yr': '2-Year and 2.5-Year Grad Rates', '3yr': '3-Year and 3.5-Year Grad Rates', '4yr': '4-Year and 4.5-Year Grad Rates', '5yr': '5-Year and 5.5-Year Grad Rates', '6yr': '6-Year and 6.5-Year Grad Rates'};
@@ -210,7 +196,6 @@
 				}
 			}
 		});
-		//console.log(JSON.stringify(options));
 		config.period = selectedval;
 		$('#dataset_filter3').html(options.join(''));
 	};
